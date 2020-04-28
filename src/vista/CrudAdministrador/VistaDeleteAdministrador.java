e6b7a059639c2732c0a505d9883cb9cd0dfad670
DiegoGrajales1229 <63883371+DiegoGrajales1229@users.noreply.github.com>
2020-04-28 08:58:20 -0500

    actualizacion Ficheros

--- src/vista/CrudAdministrador/VistaDeleteAdministrador.java
+++ src/vista/CrudAdministrador/VistaDeleteAdministrador.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package vista;
+package vista.CrudAdministrador;
 
 import control.ControlAdministrador;
 import java.util.LinkedList;
