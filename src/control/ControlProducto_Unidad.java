49ed17c21d96b8f01e0199981e20230e209820b1
JuanDied <64377186+JuanDied@users.noreply.github.com>
2020-05-04 14:54:37 -0500

    vistanuevas
    vistaeliminarycrearunidaddeproducto

--- src/control/ControlProducto_Unidad.java
+++ src/control/ControlProducto_Unidad.java
@@ -5,10 +5,57 @@
  */
 package control;
 
+import java.util.LinkedList;
+import modelo.producto_unidad;
+
 /**
  *
  * @author Diego Alejandro
  */
 public class ControlProducto_Unidad {
+    
+    
+    
+      public boolean insertarProductoUnidad(LinkedList<producto_unidad> listaProUn) {
+        boolean t=false;
+     
+        for (int i = 0; i <listaProUn.size(); i++) {
+          producto_unidad pu = listaProUn.get(i);
+            String sql="insert into producto_unidad (idServicioDf, idProductoAseof, fechaUnidadProducto) "
+                    + "value("+ pu.getIdServicioDf()+" , " + pu.getIdProductoAseof()+", '"+pu.getFechaUnidadProducto()  +"' );";
+            
+            t=pu.sqlProductoUnidad(sql);
+        }
+        
+        
+        return t;
+        
+        
+    }
+    
+    
+    
+     public LinkedList<producto_unidad> consultarProductoUnidad() {
+        LinkedList<producto_unidad> lista = new LinkedList<>();
+        producto_unidad obj = new producto_unidad();
+        String sql = "select * from producto_unidad;";
+        
+        String sql2="SELECT producto_unidad.idProductoU, productos_aseo.nombreProductoAseo npa, producto_unidad.idServicioDf untService,\n" +
+" unidades_residenciales.nombreUnidadResidencial nur, producto_unidad.fechaUnidadProducto FROM (producto_unidad \n" +
+" LEFT JOIN unidades_residenciales ur ON untService= ur.nitUnidad)\n" +
+" LEFT JOIN productos_aseo pa ON pa.idProductoAseo= producto_unidad.idProductoAseof\n" +
+" ORDER BY  idServicioDf, fechaUnidadProducto;";
+                
+        lista = obj.consultarProductoUnidad(sql);
+        return lista;
+    }
+
+    public boolean deleteProductoUnidad(int id) {
+        boolean t=false;
+        producto_unidad obj2 = new producto_unidad();
+        String sql="delete  fromproducto_unidad where idProductoU= "+id+";";
+        t=obj2.sqlProductoUnidad(sql);
+        return t;
+    }
     
 }
--- src/modelo/producto_unidad.java
+++ src/modelo/producto_unidad.java
@@ -5,6 +5,14 @@
  */
 package modelo;
 
+import control.BaseDatos;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Diego Alejandro
@@ -12,19 +20,27 @@ package modelo;
 public class producto_unidad {
     
     private int idProductoU,idServicioDf,idProductoAseof;
+    private String fechaUnidadProducto;
 
     public producto_unidad() {
     }
 
-    public producto_unidad(int idServicioDf, int idProductoAseof) {
+    public producto_unidad(int idProductoAseof, String fechaUnidadProducto,  int idServicioDf) {
         this.idServicioDf = idServicioDf;
         this.idProductoAseof = idProductoAseof;
+        this.fechaUnidadProducto=fechaUnidadProducto;
+    }
+    public producto_unidad( int idProductoAseof,  String fechaUnidadProducto) {
+        
+        this.idProductoAseof = idProductoAseof;
+        this.fechaUnidadProducto=fechaUnidadProducto;
     }
 
-    public producto_unidad(int idProductoU, int idServicioDf, int idProductoAseof) {
+    public producto_unidad(int idProductoU, int idProductoAseof, String fechaUnidadProducto, int idServicioDf) {
         this.idProductoU = idProductoU;
         this.idServicioDf = idServicioDf;
         this.idProductoAseof = idProductoAseof;
+        this.fechaUnidadProducto=fechaUnidadProducto;
     }
 
     public int getIdProductoU() {
@@ -50,11 +66,116 @@ public class producto_unidad {
     public void setIdProductoAseof(int idProductoAseof) {
         this.idProductoAseof = idProductoAseof;
     }
+    
+    
+    public String getFechaUnidadProducto() {
+        return fechaUnidadProducto;
+    }
+
+    public void setFechaUnidadProducto(String fechaUnidadProducto) {
+        this.fechaUnidadProducto = fechaUnidadProducto;
+    }
+    
 
     @Override
     public String toString() {
         return "producto_unidad{" + "idProductoU=" + idProductoU + ", idServicioDf=" + idServicioDf + ", idProductoAseof=" + idProductoAseof + '}';
     }
     
+     public boolean sqlProductoUnidad(String sql) {
+        boolean t=false;
+        BaseDatos objCon = new BaseDatos();
+
+        if (objCon.crearConexion()) {
+            try {
+                Statement sentencia = objCon.getConexion().createStatement();
+                sentencia.executeUpdate(sql);
+                t=true;
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+                t= false;
+            }
+        }
+        
+        return t;
+    }
+     
+     public LinkedList<producto_unidad> consultarProductoUnidad(String sql) {
+        LinkedList<producto_unidad> listapu = new LinkedList<>();
+        BaseDatos objb = new BaseDatos();
+        int id = 0;
+        int idPA = 0;
+        int idDS= 0;
+        String fecha="";
+        
+        ResultSet rs = null;
+        if (objb.crearConexion()) {
+            try {
+                rs = objb.getSt().executeQuery(sql);
+                while (rs.next()) {
+                    id = rs.getInt("idProductoAseo");
+                     idPA = rs.getInt("idProductoAseof");
+                     idDS = rs.getInt("idServicioDf");
+                    
+                     fecha = rs.getString("fechaUnidadProducto");
+                    
+                    
+                    listapu.add(new producto_unidad(id, idPA, fecha, idDS));
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(producto_unidad.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return listapu;
+
+    }
+
+    public LinkedList<producto_unidad> consultarProductoUnidadAdv(String sql) {
+        LinkedList<producto_unidad> listapu = new LinkedList<>();
+        BaseDatos objb = new BaseDatos();
+        int id = 0;
+        String nombrePA="";
+        int idDS= 0;
+        String nombreUR="";
+        String fecha="";
+        
+        ResultSet rs = null;
+        if (objb.crearConexion()) {
+            try {
+                rs = objb.getSt().executeQuery(sql);
+                while (rs.next()) {
+                    
+                    
+                    id = rs.getInt("idProductoAseo");
+                    nombrePA= rs.getString("npa");
+                    
+                    
+                     idDS = rs.getInt("untService");
+                      nombreUR=rs.getString("nur");
+                    
+                     fecha = rs.getString("fecha");
+                    
+                    
+                    listapu.add(new producto_unidad(id, idPA, fecha, idDS));
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(producto_unidad.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return listapu;
+
+    }
+    
+    public LinkedList<LinkedList>  busquedaAvanzada (String sql){
+    LinkedList <LinkedList>lista= new LinkedList<>();
+    LinkedList<String> subLista = new LinkedList<>();
+    
+    subLista.add("hola");
+    subLista.add("foca");
+    
+    lista.add(subLista);
+    
+    return lista;
+    }
     
 }
