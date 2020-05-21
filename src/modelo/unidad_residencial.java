e15cade6a810bcb7e726bfb4e4d28c2756a8f7e8
JuanDied <64377186+JuanDied@users.noreply.github.com>
2020-05-21 09:38:48 -0500

    NuevoCOMIT
    nuevoscambios

--- src/modelo/unidad_residencial.java
+++ src/modelo/unidad_residencial.java
@@ -5,6 +5,14 @@
  */
 package modelo;
 
+import control.BaseDatos;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Diego Alejandro
@@ -70,5 +78,50 @@ public class unidad_residencial {
         return "unidad_residencial{" + "nitUnidad=" + nitUnidad + ", nombreUnidadResidencial=" + nombreUnidadResidencial + ", fechaContratoUnidad=" + fechaContratoUnidad + ", direccionUnidad=" + direccionUnidad + ", idAdminf=" + idAdminf + '}';
     }
     
-    
+    public boolean sqlUnidadResidencial(String sql) {
+        boolean t=false;
+        BaseDatos objCon = new BaseDatos();
+
+        if (objCon.crearConexion()) {
+            try {
+                Statement sentencia = objCon.getConexion().createStatement();
+                sentencia.executeUpdate(sql);
+                t=true;
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+                t= false;
+            }
+        }
+        
+        return t;
+    }
+     
+     public LinkedList<unidad_residencial> consultarUnidadR(String sql) {
+        LinkedList<unidad_residencial> listaUR = new LinkedList<>();
+        BaseDatos objb = new BaseDatos();
+      String nit="";
+      String nombre="";
+        String      fecha="";
+           String   direccion="";
+       int idAdmi=0;
+        ResultSet rs = null;
+        if (objb.crearConexion()) {
+            try {
+                rs = objb.getSt().executeQuery(sql);
+                while (rs.next()) {
+                    nit = rs.getString(" nitUnidad");
+                    nombre = rs.getString("nombreUnidadResidencial");
+                    fecha= rs.getString("fechaContratoUnidad");
+                     direccion = rs.getString("direccionUnidad");
+                     idAdmi= rs.getInt("idAminf");
+                   
+                    listaUR.add(new unidad_residencial(nit, nombre, fecha,direccion,idAdmi));
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(productos_aseo.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return listaUR;
+
+    }
 }
